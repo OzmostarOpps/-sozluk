c6665a0137bea874e80056cd7ff133776686f8ac
Ozmostar <ozmostar77@gmail.com>
2024-12-30 21:23:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+kelimeler = {
+    "piton" : "zehirsiz bir yılan türü",
+    "laboratuvar" : "deney yapılan yer",
+    "sefa" : "keyif"
+}
+
+while True:
+    kelime = input("Bir kelime girin")
+    if kelime in list(kelimeler.keys()):
+        print(kelimeler[kelime])
+    else:
+        anlami = input("anlamını girin")
+        kelimeler[kelime] = anlami
+        print(kelime, " sözlüğe eklendi")
+        
